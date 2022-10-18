fb9499b9f836b325b802d58d3344d7164e2c2569
Aronimo Ademola <aronimoademola@gmail.com>
2022-10-18 08:08:25 -0700

    ryd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
-#include "putchar.h"
-
+#include "main.h"
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ * main - function that prints _putchar
+ * Return: returns 0 if successful
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -13,8 +13,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r')
-	_putchar('/n');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
