3a5c59480e98602e48a7baade0f6212ee783bce7
Aronimoademola <98184396+Aronimoademola@users.noreply.github.com>
2022-10-18 19:22:33 +0100

    Upd

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,6 @@ void print_times_table(int n)
 	int column;
 	int product;
 
-
 	if (n < 0 || n >= 15)
 		return;
 	for (row = 0; row <= n; row++)
@@ -17,36 +16,32 @@ void print_times_table(int n)
 		for (column = 0; column <= n; column++)
 		{
 			product = (row * column);
-l
-	if (column == 0)
-		_putchar('0' + product);
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-
-		if (product <= 9)
-		{
-
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + product);
-
+			if (column == 0)
+				_putchar('0' + product);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
+			}
 		}
-		else if (product > 9 && product < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + (product / 10));
-			_putchar('0' + (product % 10));
-		}
-		else if (product >= 100)
-		{
-			_putchar('0' + (product / 100));
-			_putchar('0' + ((product / 10) % 10));
-			_putchar('0' + (product % 10));
-		}
+		_putchar('\n');
 	}
-}
-_putchar('\n');
-}
 }
