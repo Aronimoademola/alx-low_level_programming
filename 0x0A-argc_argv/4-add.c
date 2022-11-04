19d4189c2faacf4248640af967afa760a663f38e
Aronimoademola <98184396+Aronimoademola@users.noreply.github.com>
2022-11-05 00:17:43 +0100

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,45 +1,33 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include "main.h"
 
 /**
- * main - Program that takes in all integer arguments and returns the sum
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ * main - this is a function to print its name
+ * @argc: argc param
+ * @argv: an array of a command listed
+ * Return: 0 for success
  */
-
 int main(int argc, char *argv[])
 {
-	int i, j, length, sum;
-	char *ptr;
+	int result = 0, num, i, j, k;
 
-	if (argc < 2)
-	printf("0\n");
-	else
+	for (i = 1; i < argc; i++)
 	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			ptr = argv[i];
-			length = strlen(ptr);
-
-			for (j = 0; j < length; j++)
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
-				if (isdigit(*(ptr + j)) == 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
+			printf("%s\n", "Error");
+			return (1);
 			}
-
-			sum += atoi(argv[i]);
 		}
-
-		printf("%d\n", sum);
 	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", result);
 	return (0);
 }
